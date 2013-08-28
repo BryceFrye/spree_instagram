3985bd963a5d50ad09404b8a271dc69a5c14bb19
Bryce <bcfrye19@gmail.com>
2013-08-28 16:15:05 -0700

    admin changes update asynchronously

--- app/assets/javascripts/admin/spree_instagram.js
+++ app/assets/javascripts/admin/spree_instagram.js
@@ -1,1 +1,30 @@
 //= require admin/spree_backend
+
+$(document).ready(function(){
+	
+	$(".instagram-photo a").on("click", function(e){
+		e.preventDefault();
+		$.ajax({
+			url: $(this).data("url"),
+		}).done(function(data){
+			if (data.status === "success"){
+				changeState(e);
+			}
+		})
+	});
+	
+	function changeState(e){
+		var photo = $(e.target).parent();
+		var link = $(e.target);
+		if (photo.hasClass("approved")){
+			photo.removeClass("approved").addClass("awaiting-approval");
+			link.data("url", link.data("url").replace(/revoke/, "approve"));
+			link.html("Approve");
+		} else {
+			photo.removeClass("awaiting-approval").addClass("approved");
+			link.data("url", link.data("url").replace(/approve/, "revoke"));
+			link.html("Remove");
+		}
+	}
+	
+});
--- app/controllers/spree/instagram_photos_controller.rb
+++ app/controllers/spree/instagram_photos_controller.rb
@@ -1,17 +1,22 @@
 module Spree
   class InstagramPhotosController < Spree::StoreController
-    # TODO: before_filter :authenticate
     
     def approve
       @photo = Spree::InstagramPhoto.find(params[:photo])
-      @photo.update_attributes(approved: true)
-      redirect_to admin_instagram_path
+      if @photo.update_attributes(approved: true)
+        render json: { status: :success }
+      else
+        render json: { status: :failure }
+      end
     end
     
     def revoke_approval
       @photo = Spree::InstagramPhoto.find(params[:photo])
-      @photo.update_attributes(approved: false)
-      redirect_to admin_instagram_path
+      if @photo.update_attributes(approved: false)
+        render json: { status: :success }
+      else
+        render json: { status: :failure }
+      end
     end
     
   end
